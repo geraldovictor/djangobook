78f63d99250bec1579e55b83549907b9194e07f8
Geraldo <geraldovictor@outlook.com>
2019-10-01 12:25:00 -0300

    continuação

--- bookproject/bdbook/models.py
+++ bookproject/bdbook/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from rest_framework import viewsets
 # Create your models here.
 
+class BookNumber(models.Model):
+    isbn_10 = CharField(max_length=10, blank=True)
+    isbn_13 = CharField(max_length=13, blank=True)
+
 class Book(models.Model):
     title = models.CharField(max_length=36, unique=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     desc = models.TextField(max_length=256, blank=True)
     date = models.DateField(auto_now_add=True)
+    number = models.OneToOneField(BookNumber,null=True,blank=True,on_delete= models.CASCADE)
     def __str__(self):
         return self.title
 
